b319421e250f6ac8c7620672352e59137d665cf2
Bodhaditya19 <mukherjee.bodhaditya@gmail.com>
2021-08-24 17:36:04 +0530

    24/8/2021

--- basics2/cardEx2.js
+++ basics2/cardEx2.js
@@ -2,6 +2,36 @@
  * Making a  deck of cards using nested for loop
  */
 
+// const cardSuit = ["Spades", "Clubs", "Hearts", "Diamonds"];
+
+// const values = [
+//     "Ace", 2, 3, 4, 5, 6, 7, 8, 9, "King", "Queen", "Jack"
+// ];
+// let group = [];
+// let deck =  [];
+
+// for(let i = 0; i<cardSuit.length; i++){
+//     for(let x = 0; x < values.length; x++){
+//         let card = {
+//             Value : values[x],
+//             Suit  : cardSuit[i],
+//         }
+//         group.push(card);
+
+//     }
+//     deck.push(group);
+
+// }
+// console.log(deck[0]);
+
+
+
+
+
+
+
+
+
 const cardSuit = ["Spades", "Clubs", "Hearts", "Diamonds"];
 
 const values = [
@@ -20,6 +50,7 @@ for(let i = 0; i<cardSuit.length; i++){
 
     }
     deck.push(group);
+    group = null;
 
 }
-console.log(deck[0]);
+console.log(deck);
